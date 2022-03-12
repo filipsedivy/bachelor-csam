3f7256f903b49167ecf16e8f52c516b076cc5e99
Filip Sedivy <mail@filipsedivy.cz>
2022-03-12 02:07:35 +0100

    Add model application

---
+++ toolkits/application.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+
+def get_labels():
+    """Getting labels
+    Return:
+        List of labels
+    """
+    return (
+        'adult', 'adult_half_naked', 'adult_naked',
+        'child', 'child_half_naked', 'child_naked',
+        'nature', 'other'
+    )
+
+
+def decode_predictions(preds) -> list:
+    """Decoding the prediction
+    Args:
+        preds: Prediction output from the model
+    Return:
+        Mapping predictions to labels
+        [(label, score), (label, score)]
+    """
+    if type(preds) is np.ndarray:
+        preds = preds[0]
+
+    result = list()
+    labels = get_labels()
+
+    for i, prediction in enumerate(preds):
+        result.append((labels[i], prediction))
+
+    return result
